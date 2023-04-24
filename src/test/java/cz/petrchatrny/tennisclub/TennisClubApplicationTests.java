bc3dfcd6684c45e8c4cf6c6ce3e808227c4a5dfe
Petr Chatrný <p.chatrny@protonmail.com>
2023-04-24 10:13:06 +0200

    init

---
+++ src/test/java/cz/petrchatrny/tennisclub/TennisClubApplicationTests.java
@@ -0,0 +1,13 @@
+package cz.petrchatrny.tennisclub;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TennisClubApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
