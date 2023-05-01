30c4739741c184fe24795c4c03834345259e36b2
Petr Chatrný <p.chatrny@protonmail.com>
2023-05-02 01:23:35 +0200

    add Court controller layer

---
+++ src/main/java/cz/petrchatrny/tennisclub/config/OpenApiConfig.java
@@ -0,0 +1,30 @@
+package cz.petrchatrny.tennisclub.config;
+
+import io.swagger.v3.oas.models.Components;
+import io.swagger.v3.oas.models.OpenAPI;
+import io.swagger.v3.oas.models.info.Contact;
+import io.swagger.v3.oas.models.info.Info;
+import io.swagger.v3.oas.models.security.SecurityScheme;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+public class OpenApiConfig {
+    public static final String SECURITY_SCHEME = "bearer-jwt";
+    private final SecurityScheme jwtScheme = new SecurityScheme()
+            .type(SecurityScheme.Type.HTTP)
+            .scheme("bearer")
+            .bearerFormat("JWT");
+
+    @Bean
+    public OpenAPI customOpenAPI() {
+        return new OpenAPI()
+                .components(new Components().addSecuritySchemes(SECURITY_SCHEME, jwtScheme))
+                .info(new Info()
+                        .title("Tennis club")
+                        .version("1.0")
+                        .description("This project is a Java assignment from inQool.")
+                        .contact(new Contact().name("developer - Petr Chatrný").email("p.chatrny@protonmail.com"))
+                );
+    }
+}
---
+++ src/main/java/cz/petrchatrny/tennisclub/controller/CourtController.java
@@ -0,0 +1,80 @@
+package cz.petrchatrny.tennisclub.controller;
+
+import cz.petrchatrny.tennisclub.dto.court.CreateCourtDTO;
+import cz.petrchatrny.tennisclub.dto.court.ResponseCourtDTO;
+import cz.petrchatrny.tennisclub.service.CourtService;
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.media.ArraySchema;
+import io.swagger.v3.oas.annotations.media.Content;
+import io.swagger.v3.oas.annotations.media.Schema;
+import io.swagger.v3.oas.annotations.responses.ApiResponse;
+import io.swagger.v3.oas.annotations.responses.ApiResponses;
+import io.swagger.v3.oas.annotations.tags.Tag;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.Collection;
+
+@Tag(name = "courts")
+@RestController
+@RequestMapping("/api/v1/courts")
+public class CourtController {
+    private final CourtService courtService;
+
+    public CourtController(CourtService courtService) {
+        this.courtService = courtService;
+    }
+
+    @GetMapping("/")
+    @Operation(summary = "Get list of all courts")
+    @ApiResponses(value = {
+            @ApiResponse(responseCode = "200", description = "Success", content = {
+                    @Content(
+                            array = @ArraySchema(schema = @Schema(implementation = ResponseCourtDTO.class)),
+                            mediaType = "application/json"
+                    )
+            }),
+            @ApiResponse(responseCode = "401", description = "Unauthenticated access", content = @Content)
+    })
+    public ResponseEntity<Collection<ResponseCourtDTO>> getCourts() {
+        return new ResponseEntity<>(courtService.getCourts(), HttpStatus.OK);
+    }
+
+    @PostMapping("/")
+    @Operation(summary = "Create new court")
+    @ApiResponses(value = {
+            @ApiResponse(responseCode = "201", description = "Court created", content = @Content(mediaType = "application/json")),
+            @ApiResponse(responseCode = "401", description = "Unauthenticated access", content = @Content),
+            @ApiResponse(responseCode = "403", description = "Unauthorized access", content = @Content),
+            @ApiResponse(responseCode = "404", description = "Surface not found", content = @Content)
+    })
+    public ResponseEntity<ResponseCourtDTO> createCourt(@RequestBody CreateCourtDTO dto) {
+        return new ResponseEntity<>(courtService.createCourt(dto), HttpStatus.CREATED);
+    }
+
+    @PutMapping("/{id}")
+    @Operation(summary = "Update court")
+    @ApiResponses(value = {
+            @ApiResponse(responseCode = "202", description = "Court updated", content = @Content(mediaType = "application/json")),
+            @ApiResponse(responseCode = "401", description = "Unauthenticated access", content = @Content),
+            @ApiResponse(responseCode = "403", description = "Unauthorized access", content = @Content),
+            @ApiResponse(responseCode = "404", description = "Court not found / Surface not found", content = @Content),
+    })
+    public ResponseEntity<ResponseCourtDTO> updateCourt(@PathVariable Long id, @RequestBody CreateCourtDTO dto) {
+        return new ResponseEntity<>(courtService.updateCourt(id, dto), HttpStatus.ACCEPTED);
+    }
+
+    @DeleteMapping("/{id}")
+    @Operation(summary = "Delete court")
+    @ApiResponses(value = {
+            @ApiResponse(responseCode = "204", description = "Court deleted"),
+            @ApiResponse(responseCode = "401", description = "Unauthenticated access"),
+            @ApiResponse(responseCode = "403", description = "Unauthorized access"),
+            @ApiResponse(responseCode = "404", description = "Court not found")
+    })
+    public ResponseEntity<Void> deleteCourt(@PathVariable Long id) {
+        courtService.deleteCourt(id);
+        return new ResponseEntity<>(HttpStatus.NO_CONTENT);
+    }
+}
