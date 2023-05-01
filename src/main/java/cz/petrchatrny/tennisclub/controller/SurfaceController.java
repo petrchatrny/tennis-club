e4a58b0341d0c7f3fdcdcdc422bbd61314e41fdd
Petr Chatrný <p.chatrny@protonmail.com>
2023-05-02 01:39:18 +0200

    add Surface controller layer

---
+++ src/main/java/cz/petrchatrny/tennisclub/controller/SurfaceController.java
@@ -0,0 +1,73 @@
+package cz.petrchatrny.tennisclub.controller;
+
+import cz.petrchatrny.tennisclub.dto.surface.CreateSurfaceDTO;
+import cz.petrchatrny.tennisclub.dto.surface.ResponseSurfaceDTO;
+import cz.petrchatrny.tennisclub.service.SurfaceService;
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.media.Content;
+import io.swagger.v3.oas.annotations.responses.ApiResponse;
+import io.swagger.v3.oas.annotations.responses.ApiResponses;
+import io.swagger.v3.oas.annotations.tags.Tag;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.Collection;
+
+@Tag(name = "surfaces")
+@RestController
+@RequestMapping("/api/v1/surfaces")
+public class SurfaceController {
+    private final SurfaceService surfaceService;
+
+    public SurfaceController(SurfaceService surfaceService) {
+        this.surfaceService = surfaceService;
+    }
+
+    @GetMapping("/")
+    @Operation(summary = "Get list of surfaces")
+    @ApiResponses(value = {
+            @ApiResponse(responseCode = "200", description = "Success", content = @Content(mediaType = "application/json")),
+            @ApiResponse(responseCode = "401", description = "Unauthenticated access", content = @Content)
+    })
+    public ResponseEntity<Collection<ResponseSurfaceDTO>> getSurfaces() {
+        return new ResponseEntity<>(surfaceService.getSurfaces(), HttpStatus.OK);
+    }
+
+    @PostMapping("/")
+    @Operation(summary = "Create new surface")
+    @ApiResponses(value = {
+            @ApiResponse(responseCode = "201", description = "Surface created", content = @Content(mediaType = "application/json")),
+            @ApiResponse(responseCode = "401", description = "Unauthenticated access", content = @Content),
+            @ApiResponse(responseCode = "403", description = "Unauthorized access", content = @Content)
+    })
+    public ResponseEntity<ResponseSurfaceDTO> createCourt(@RequestBody CreateSurfaceDTO dto) {
+        return new ResponseEntity<>(surfaceService.createSurface(dto), HttpStatus.CREATED);
+    }
+
+    @PutMapping("/{id}")
+    @Operation(summary = "Update surface")
+    @ApiResponses(value = {
+            @ApiResponse(responseCode = "202", description = "Surface updated", content = @Content(mediaType = "application/json")),
+            @ApiResponse(responseCode = "401", description = "Unauthenticated access", content = @Content),
+            @ApiResponse(responseCode = "403", description = "Unauthorized access", content = @Content),
+            @ApiResponse(responseCode = "404", description = "Surface not found", content = @Content)
+    })
+    public ResponseEntity<ResponseSurfaceDTO> updateCourt(@PathVariable Long id, @RequestBody CreateSurfaceDTO dto) {
+        return new ResponseEntity<>(surfaceService.updateSurface(id, dto), HttpStatus.ACCEPTED);
+    }
+
+    @DeleteMapping("/{id}")
+    @Operation(summary = "Delete surface")
+    @ApiResponses(value = {
+            @ApiResponse(responseCode = "204", description = "Surface deleted"),
+            @ApiResponse(responseCode = "401", description = "Unauthenticated access"),
+            @ApiResponse(responseCode = "403", description = "Unauthorized access"),
+            @ApiResponse(responseCode = "404", description = "Surface not found"),
+            @ApiResponse(responseCode = "409", description = "Surface is dependency for a Court")
+    })
+    public ResponseEntity<Void> deleteCourt(@PathVariable Long id) {
+        surfaceService.deleteSurface(id);
+        return new ResponseEntity<>(HttpStatus.NO_CONTENT);
+    }
+}
